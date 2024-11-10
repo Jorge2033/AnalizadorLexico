c33bb889cbb42c3f31889d5f429537cb8e0468d5
josdramo <josdramo@espol.edu.ec>
2024-11-09 19:55:43 -0500

    Implementación de logs y codigo de prueba para expresiones aritmeticas y booleanas

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import ply.lex as lex
+import datetime
+import os
 
 tokens = (
-
+  'VARIABLE',
+  'NUMBER',
   #------Inicio: José Ramos (Operadores Arítmeticos) ------
   'PLUS',
   'MINUS',
@@ -56,7 +59,18 @@ t_OR = r'\|\|'
 t_NOT = r'!'
 #--Fin: José Ramos (Expresiones regulares Op. Booleanos) --
 
+def t_NUMBER(t):
+  r'\d+'
+  t.value = int(t.value)
+  return t
 
+def t_VARIABLE(t):
+  r'[a-zA-Z_][a-zA-Z_0-9]*'   
+  return t
+
+def t_newline(t):
+  r'\n+'
+  t.lexer.lineno += len(t.value)
 
 t_ignore = ' \t'
 
@@ -64,5 +78,28 @@ def t_error(t):
   print(f"Token no admitido '{t.value[0]}' en la línea {t.lineno}")
   t.lexer.skip(1)
 
-# Crear el analizador léxico
 lexer = lex.lex()
+
+#--Sección de prueba: José Ramos (Codigo de Prueba) --
+prueba = """
+numero != 20
+"""
+
+# Generar el nombre del archivo log
+usuario_git = "nombreDeUsuario"  # Cambia esto por tu nombre de usuario
+timestamp = datetime.datetime.now().strftime("%d%m%Y-%Hh%M")
+log_directory = "logs"
+os.makedirs(log_directory, exist_ok=True)  # Crea la carpeta 'logs' si no existe
+log_filename = os.path.join(log_directory, f"lexico-{usuario_git}-{timestamp}.txt")
+
+with open(log_filename, 'w') as log_file:
+    log_file.write("Tokens reconocidos:\n")
+    lexer.input(prueba)
+
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        log_file.write(f"{repr(tok)}\n")
+      
+print(f"El análisis léxico se ha guardado en el archivo {log_filename}")
